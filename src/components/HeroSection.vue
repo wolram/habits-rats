8e567245e0c4e04ba89ff913aee6690332a1e585
Marlow Sousa <90123576+wolram@users.noreply.github.com>
2026-01-25 15:33:15 -0600

    fix: resolve carousel height collapse by removing redundant height property

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -136,7 +136,6 @@ function triggerConfetti() {
 .phone-screen {
   position: relative;
   width: 100%;
-  height: 100%;
   background: #000;
   display: flex; /* Remove espaços brancos extras */
 }
